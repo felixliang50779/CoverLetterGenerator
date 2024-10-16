958cf10160651251d16305624239227110fae42d
Felix Liang <unset>
2024-10-15 21:01:51 -0400

    work in-progress DB synchronization

--- generator-ui/src/App.jsx
+++ generator-ui/src/App.jsx
@@ -1,5 +1,5 @@
 // External Modules
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Buffer } from 'buffer';
 import { saveAs } from 'file-saver';
 import mammoth from 'mammoth';
@@ -8,7 +8,7 @@ import mammoth from 'mammoth';
 import './App.css';
 import './index.css';
 
-async function processFile(file, fileReader) {
+async function parseFile(file, fileReader) {
   const regex = /%t(.*?)%t/g;
 
   return new Promise((resolve, reject) => {
@@ -55,20 +55,17 @@ async function convertBase64(file, fileReader) {
   });
 }
 
-async function getTemplateTargets(file) {
-
-  const fileReader = new FileReader();
-
-  const targets = await processFile(file, fileReader);
-  const base64file = await convertBase64(file, fileReader);
+async function generateFile(file, fileReader, targets) {
 
   // Manually setting replacement text here for testing
   const replacementTexts = ['10/13/2024', 'McDonalds', 'Deloitte', 'We hate everything', "Human Resources", "HR Intern", "Mom and Dad", "HR Firing Squad"];
-  
+
+
+  // Mapping to the wrong targets here as objects don't preserve order
+  // working version will use 'targets' itself as the replacementObj so should be ok
   const replacementObj = {};
-  for (let i = 0; i < targets.length; i++) {
-    const processedTarget = targets[i].slice(2, -2);
-    replacementObj[processedTarget] = replacementTexts[i];
+  for (let i = 0; i < Object.keys(targets).length ; i++) {
+    replacementObj[Object.keys(targets)[i]] = replacementTexts[i];
   }
 
   const response = await fetch("http://localhost:8080/generateFileHandler", {
@@ -76,7 +73,7 @@ async function getTemplateTargets(file) {
       "Content-Type": "application/json"
     },
     method: "POST",
-    body: JSON.stringify({ data: replacementObj, template: base64file })
+    body: JSON.stringify({ data: replacementObj, template: file.data })
   });
 
   const base64Response = (await response.json()).completedFile;
@@ -84,25 +81,104 @@ async function getTemplateTargets(file) {
   const fileBlob = new Blob ([buffer], 
     { type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" });
 
-  await saveAs(fileBlob, `Modified ${file.name}`);
+  saveAs(fileBlob, `Modified ${file.name}`);
+}
+
+async function getTemplateTargets(file, fileReader) {
+  // Get raw targets from file template
+  const targetArray = await parseFile(file, fileReader);
+
+  // Strip the '%t' indicators
+  const processedTargetArray = targetArray.map(target => target.slice(2, -2));
+
+  // Reduce the array into an object with empty mappings
+  const targetObj = processedTargetArray.reduce((obj, key) => {
+    obj[key] = "";
+    return obj;
+  }, {});
+
+  return [processedTargetArray, targetObj];
 }
 
 export default function App() {
   // Constants
-  const PLACEHOLDER_FILE = { name: "Choose a File (.docx)" };
+  const PLACEHOLDER_FILE = { name: "Choose a File (.docx)", data: {} };
+  const FILE_READER = new FileReader();
 
   const [currentFile, setCurrentFile] = useState(PLACEHOLDER_FILE);
+  const [templateTargets, setTemplateTargets] = useState({});
+
+  // will include 'currentlySelected' and 'numTargets' properties
+  const [templateMetadata, setTemplateMetadata] = useState({});
+
+
+  const initializeTemplating = async () => {
+    const [targetArray, targetObj] = await getTemplateTargets(currentFile.data, FILE_READER);
+    const dbUpdates = [];
+
+    // Convert file to base64 for storage
+    const base64File = await convertBase64(currentFile.data, FILE_READER);
+
+    // Update DB
+    dbUpdates.push(chrome.storage.session.set({ currentFile: { name: currentFile.name, data: base64File } }));
+    dbUpdates.push(chrome.storage.session.set({ templateTargets: targetObj }));
+    dbUpdates.push(chrome.storage.session.set(
+      { templateMetadata: { currentlySelected: targetArray[0], numTargets: targetArray.length } }));
+
+
+    // Resolve DB updates concurrently
+    await Promise.all(dbUpdates);
+
+    // TO-DO: This will no longer be needed once onChange handler
+    // is set-up to update UI on DB changes
+    setTemplateTargets(targetObj);
+    setTemplateMetadata({ currentlySelected: targetArray[0], numTargets: targetArray.length });
+  }
+
+  // Every time the extension popup is opened
+  useEffect(async () => {
+    // Initialize UI state using data from DB
+    chrome.storage.session.get(["currentFile", "templateTargets", "templateMetadata"], result => {
+      result.currentFile !== undefined ? setCurrentFile(result.currentFile) : setCurrentFile(PLACEHOLDER_FILE);
+      result.templateTargets !== undefined ? setTemplateTargets(result.templateTargets) : setTemplateTargets({});
+      result.templateMetadata !== undefined ? setTemplateMetadata(result.templateMetadata) : setTemplateMetadata({});
+    });
+
+    // Attach listener for future storage onChange events
+    chrome.storage.session.onChanged.addListener((changes, namespace) => {
+      if ("currentFile" in changes) {
+
+      }
+      if ("templateTargets" in changes) {
+
+      }
+      if ("templateMetadata" in changes) {
+
+      }
+    });
+  }, []);
 
   return (
     <div className="container">
       <h1>COVER LETTER GENERATOR</h1>
       <label for="file-upload" className="button">{currentFile.name}</label>
+
+      {/*
+        TO-DO: Change ternary operands to set DB values instead of state -
+        onChange handler should take care of UI updates
+      */}
       <input id="file-upload" type="file" accept=".docx" onChange={event => {
-        event.target.files[0] ? setCurrentFile(event.target.files[0]) : setCurrentFile(PLACEHOLDER_FILE);
+        event.target.files[0] ? setCurrentFile({ name: event.target.files[0].name, data: event.target.files[0] }) : 
+          setCurrentFile(PLACEHOLDER_FILE);
       }}/>
       <span id="spacer" />
-      <button className={Object.keys(currentFile).length === 1 ? 'hidden confirm-button' : 'confirm-button'} onClick={() => getTemplateTargets(currentFile)}>
-        Parse
+      <button
+        className={currentFile.name === PLACEHOLDER_FILE.name ? 'hidden confirm-button' : 'confirm-button'}
+        onClick={initializeTemplating}>
+          Parse
+      </button>
+      <button onClick={() => generateFile(currentFile, FILE_READER, templateTargets)}>
+        Generate
       </button>
     </div>
   )
