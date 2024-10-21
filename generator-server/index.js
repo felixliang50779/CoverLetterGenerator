29de173fe83b4ce7bb2d8ec44e9d789443828314
Felix Liang <unset>
2024-10-21 11:12:46 -0400

    backend deployment configs and ui fix

--- generator-server/index.js
+++ generator-server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'chrome-extension://bcbacpbjnaofjickhkhlcpemelfigipn')
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Methods, Credentials')
