e59f7088805966062eba01e578a47044ffef1b57
Felix Liang <unset>
2024-11-28 20:34:34 -0500

    another revert for the orphaned tooltip fix

--- generator-ui/content-script/src/main.jsx
+++ generator-ui/content-script/src/main.jsx
@@ -8,6 +8,12 @@ root.id = "floating-tooltip";
 root.style.position = "fixed";
 root.style.zIndex = "9999";  // making sure the tooltip isn't covered by any elements
 
+// if orphaned script is present we remove its associated tooltip element
+const orphaned_element = document.getElementById("floating-tooltip");
+if (orphaned_element) {
+  orphaned_element.remove();
+}
+
 document.body.parentNode.insertBefore(root, document.body);
 
 createRoot(root).render(
