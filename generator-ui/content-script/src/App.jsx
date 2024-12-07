fdbabded86de24cde8a2d10e374fec787db27708
Felix Liang <unset>
2024-12-07 17:25:06 -0500

    back to local storage for tooltip coords

--- generator-ui/content-script/src/App.jsx
+++ generator-ui/content-script/src/App.jsx
@@ -15,15 +15,23 @@ export default function App() {
     // when content script is first loaded
     useEffect(() => {
         // load values for tooltip position, templatedItems, and currentlySelected from storage into state vars
-        chrome.storage.session.get(["templateTargets", "currentlySelected", "tooltipCoords"], result => {
-            result.templateTargets !== undefined ? setTemplateTargets(result.templateTargets) : setTemplateTargets({});
-            result.currentlySelected !== undefined ? setCurrentlySelected(result.currentlySelected) : 
-                setCurrentlySelected("");
+        chrome.storage.local.get(["tooltipCoords"], result => {
             result.tooltipCoords !== undefined ? setTooltipCoords(result.tooltipCoords) : 
                 setTooltipCoords(INITIAL_POSITION);
         });
+        chrome.storage.session.get(["templateTargets", "currentlySelected"], result => {
+            result.templateTargets !== undefined ? setTemplateTargets(result.templateTargets) : setTemplateTargets({});
+            result.currentlySelected !== undefined ? setCurrentlySelected(result.currentlySelected) : 
+                setCurrentlySelected("");
+        });
 
         // add listeners for storage changes to all of the above to propagate state
+        chrome.storage.local.onChanged.addListener((changes, namespace) => {
+            if ("tooltipCoords" in changes) {
+                changes.tooltipCoords.newValue !== undefined ? setTooltipCoords(changes.tooltipCoords.newValue) :
+                    setTooltipCoords(INITIAL_POSITION);
+            }
+        });
         chrome.storage.session.onChanged.addListener((changes, namespace) => {
             if ("templateTargets" in changes) {
                 changes.templateTargets.newValue !== undefined ? 
@@ -32,10 +40,6 @@ export default function App() {
             if ("currentlySelected" in changes) {
                 changes.currentlySelected.newValue !== undefined ? setCurrentlySelected(changes.currentlySelected.newValue) :
                     setCurrentlySelected("");
-            }
-            if ("tooltipCoords" in changes) {
-                changes.tooltipCoords.newValue !== undefined ? setTooltipCoords(changes.tooltipCoords.newValue) :
-                    setTooltipCoords(INITIAL_POSITION);
             }
         });
 
@@ -55,7 +59,7 @@ export default function App() {
             defaultPosition={tooltipCoords}
             bounds="html"
             cancel={"input"}
-            onStop={(e, data) => chrome.storage.session.set({ tooltipCoords: { x: data.x, y: data.y } }) }>
+            onStop={(e, data) => chrome.storage.local.set({ tooltipCoords: { x: data.x, y: data.y } }) }>
                 <div>
                     <FloatInput
                         target={currentlySelected}
